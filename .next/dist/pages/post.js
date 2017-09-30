81f610d7ae7d32ca6f38cf5f6573b7650f75129d
nontachaiwebdev <sutininter@gmail.com>
2017-09-30 23:11:30 +0700

    feat(allPage): Add All Page

---
+++ .next/dist/pages/post.js
@@ -0,0 +1,37 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+   value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _MyLayout = require('../components/MyLayout.js');
+
+var _MyLayout2 = _interopRequireDefault(_MyLayout);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/nontchai/Desktop/lotto-ui/pages/post.js?entry';
+
+exports.default = function (props) {
+   return _react2.default.createElement(_MyLayout2.default, {
+      __source: {
+         fileName: _jsxFileName,
+         lineNumber: 4
+      }
+   }, _react2.default.createElement('h1', {
+      __source: {
+         fileName: _jsxFileName,
+         lineNumber: 5
+      }
+   }, props.url.query.title), _react2.default.createElement('p', {
+      __source: {
+         fileName: _jsxFileName,
+         lineNumber: 6
+      }
+   }, 'This is the blog post content.'));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL3Bvc3QuanMiXSwibmFtZXMiOlsiTGF5b3V0IiwicHJvcHMiLCJ1cmwiLCJxdWVyeSIsInRpdGxlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxVQUFBLEFBQUMsT0FBRDswQkFDWCxBQUFDOzttQkFBRDtxQkFBQSxBQUNHO0FBREg7QUFBQSxJQUFBLGtCQUNHLGNBQUE7O21CQUFBO3FCQUFBLEFBQUs7QUFBTDtBQUFBLFlBQUssQUFBTSxJQUFOLEFBQVUsTUFEbEIsQUFDRyxBQUFxQixBQUNyQix3QkFBQSxjQUFBOzttQkFBQTtxQkFBQTtBQUFBO0FBQUEsTUFIUSxBQUNYLEFBRUc7QUFIUCIsImZpbGUiOiJwb3N0LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9ub250Y2hhaS9EZXNrdG9wL2xvdHRvLXVpIn0=
